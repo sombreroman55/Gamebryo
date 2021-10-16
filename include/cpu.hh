44d9631009d47617f73009c05f5c22bbb1b6a19d
Andrew Roberts <sombreroman55@gmail.com>
2021-10-16 02:21:05 -0600

    Starting CPU work

---
+++ include/cpu.hh
@@ -0,0 +1,21 @@
+/* cpu.hh
+ *
+ * Gameboy CPU interface
+ *
+ *
+ */
+
+#ifndef _CPU_H_
+#define _CPU_H_
+
+#include <cstdint>
+
+class CPU
+{
+private:
+public:
+  CPU();
+  ~CPU();
+};
+
+#endif /* _CPU_H_ */
---
+++ src/lr35902.cc
@@ -0,0 +1,10 @@
+/* lr35902.cc
+ *
+ * Gameboy CPU implementation
+ *
+ *
+ */
+
+#include "cpu.hh"
+
+CPU::CPU() {}
