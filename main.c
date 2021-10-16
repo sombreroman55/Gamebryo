709a78a86575008c2c1f133cb06173499a6b1f7d
Andrew Roberts <sombreroman55@gmail.com>
2021-10-16 01:25:04 -0600

    Adding Dear ImGui as a submodlue

--- main.c
+++ main.c
@@ -4,11 +4,18 @@
 
 //#include "system.h"
 #include <SDL2/SDL.h>
-#include "cpu.h"
 
 int main (int argc, char** argv)
 {
-    CPU cpu;
-    cpu_init(&cpu);
+    SDL_version compiled;
+    SDL_version linked;
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+    printf("We compiled against SDL version %d.%d.%d ...\n",
+                   compiled.major, compiled.minor, compiled.patch);
+    printf("But we are linking against SDL version %d.%d.%d.\n",
+                   linked.major, linked.minor, linked.patch);
+    
     return 0;
 }
